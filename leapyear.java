756b653a0ad6a91a3781a304ade92b64096a850f
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-02-15 23:27:30 +0530

    Add files via upload

---
+++ leapyear.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class leapyear {
+    public static void main(String args[]){
+      System.out.println("enter leap year");
+      Scanner sc=new Scanner(System.in);
+      int year=sc.nextInt();
+      if((year%4==0)&&(year%100!=0)||(year%400==0))
+      {
+        System.out.println("Leap year");
+      }
+      else{
+        System.out.println("not leap year");
+      }
+    }
+
+}
