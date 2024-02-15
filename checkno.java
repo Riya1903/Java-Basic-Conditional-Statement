934a1531517b088586d69314ef7cf18eee41e336
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-02-15 23:23:55 +0530

    Add files via upload

---
+++ checkno.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class checkno {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number");
+        int number=sc.nextInt();
+        if(number>0){
+            System.out.println("number is positive");
+        }
+        else
+        {
+            System.out.println("number is negative");
+        }
+    }
+
+}
