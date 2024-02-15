8246e23a6b2da1198e2ddcf50a649d431c7a1e2a
Riya Pawar <100956741+Riya1903@users.noreply.github.com>
2024-02-15 23:28:27 +0530

    Add files via upload

---
+++ maxof3no.java
@@ -0,0 +1,19 @@
+public class maxof3no {
+    public static void main(String args[])
+    {
+        int a=1;
+        int b=3;
+        int c=5;
+        if((a>=b)&& (a>=c))
+        {
+            System.out.println("a is greater");
+        }
+        else if(b>=c){
+            System.out.println("b is greater");
+        }
+        else{
+            System.out.println("c is greater");
+        }
+    }
+
+}
